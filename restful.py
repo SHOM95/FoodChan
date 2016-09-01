d92369fc90ba8c249e6efd268892dde2e82c7954
PineApple777 <PineApple777@github.com>
2016-09-01 13:56:53 +0900

    first commit

--- restful.py
+++ restful.py
@@ -1,10 +1,16 @@
-from flask import Flask, jsonify, request   #import objects from the Flask model
+from flask import Flask, jsonify, request, render_template   #import objects from the Flask model
 app = Flask(__name__)   #define app using Flask
 
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({'message' : 'It works!'})
+@app.route('/home',methods=['GET'])
+def home():
+    return 'This page is home page'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)  #run app on port 8080 in debug mode
+
 
