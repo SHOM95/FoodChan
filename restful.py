2fc3630e94f249419fb68d6d5e74e1909e4e791d
PineApple777 <PineApple777@github.com>
2016-09-01 14:22:27 +0900

    render_template

--- restful.py
+++ restful.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, request, render_template   #import objects from the Flask model
+from datetime import datetime
 app = Flask(__name__)   #define app using Flask
 
 @app.route('/', methods=['GET'])
-def test():
-    return jsonify({'message' : 'It works!'})
+#def test():
+#    return jsonify({'message' : 'It works!'})
 @app.route('/home',methods=['GET'])
 def home():
-    return 'This page is home page'
+    return render_template('index.html',title='Home Page',year=datetime.now().year)
 
 
 
